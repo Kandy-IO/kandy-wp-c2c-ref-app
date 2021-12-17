28ba2b8fe2067a525ad810c2ea66d4581c9caa4c
Rahul <ribbon-ragu>
2021-12-17 22:06:27 +0530

    remove custom parameter

--- C2C_CustomerApp/C2C_CustomerAppSDK/js/comm/outcall.js
+++ C2C_CustomerApp/C2C_CustomerAppSDK/js/comm/outcall.js
@@ -42,8 +42,8 @@ export class OutCall {
   }
   start() {
     console.log('OutCall.start, AccountToken:', Model.callCredentials.accountToken)
-    if(Model.callOptions.customParameters[0].value.length < 3 || Model.callOptions.customParameters[1].value.length < 3) {
-        delete Model.callOptions.customParameters
+    if ((Model.callOptions.customParameters) && (Model.callOptions.customParameters[0].value.length < 3 || Model.callOptions.customParameters[1].value.length < 3)) {
+      delete Model.callOptions.customParameters
     }
     this.callId = this.client.call.makeAnonymous(Model.callee, Model.callCredentials, Model.callOptions)
     console.log('OutCall.start, Call ID is', this.callId)
